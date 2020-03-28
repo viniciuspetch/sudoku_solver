5bcf9a4fa5d16d0c5b8f78f9e98855eb176041a8
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2020-03-28 19:32:30 -0300

    UI improvements

--- ui.py
+++ ui.py
@@ -1,11 +1,38 @@
-from tkinter import *
-from tkinter.ttk import *
+import tkinter as tk
+import tkinter.ttk as ttk
+from tkinter import filedialog
 import main
 
 def solveBtnAction():
     statusLabelVar.set("Solving...")
     root.update()
     solve()
+
+def inputFileBtnAction():
+    filename = filedialog.askopenfilename(initialdir="./", title="Select file")
+    with open(filename, "r") as inputFile:
+        inputList = inputFile.readlines()
+        for s in inputList:
+            if s == "":
+                inputList.remove(s)
+        for i in range(9):
+            newList = inputList[i].replace(" ", "")
+            for j in range(9):
+                scell[j][i].delete(0, tk.END)
+                if newList[j] != '0':
+                    scell[j][i].insert(0, newList[j])
+        statusLabelVar.set("File opened")
+
+def outputFileBtnAction():
+    filename = filedialog.asksaveasfilename(initialdir="./", title="Select file")
+    with open(filename, "w") as outputFile:
+        output = ''
+        for i in range(9):
+            for j in range(9):
+                output += scell[i][j].get()
+            output += '\n'
+        outputFile.write(output)
+        statusLabelVar.set("File saved")
 
 def solve():    
     solution = []
@@ -15,31 +42,56 @@ def solve():
             if scell[i][j].get() == '':
                 solution[i].append('0')
             else:
+                scell[i][j]['background'] = 'light grey'
                 solution[i].append(scell[i][j].get())
+    root.update()           
     result = main.main(solution, algorithm=algoCombobox.get()).toMatrix()
     for i in range(9):
         for j in range(9):
-            scell[i][j].delete(0, END)
+            scell[i][j].delete(0, tk.END)
             scell[i][j].insert(0, result[i][j])
     statusLabelVar.set("Solved!")
 
 
-root = Tk()
-frame1 = Frame(root)
-frame1.grid(column=0, row=0)
+root = tk.Tk()
+mainframe = tk.Frame(root)
+mainframe.grid(column=0, row=0)
+frame1 = tk.Frame(mainframe)
+frame2 = tk.Frame(mainframe)
+frame1.grid(column=0, row=0, sticky='w')
+frame2.grid(column=0, row=1, sticky='w')
+
+group = []
+for i in range(3):
+    group.append([])
+    for j in range(3):
+        group[i].append(ttk.Labelframe(frame1))
+        group[i][j].grid(column=i, row=j)
+
 scell = []
 for i in range(9):
     scell.append([])
     for j in range(9):
-        scell[i].append(Entry(frame1, width='4'))
-        scell[i][j].grid(column=i, row=j)
-solveBtn = Button(frame1, text="Solve", command=solveBtnAction)
-solveBtn.grid(column=0, row=9, columnspan=3)
-statusLabelVar = StringVar()
-statusLabel = Label(frame1, textvariable=statusLabelVar)
-statusLabel.grid(column=0, row=10, columnspan=4)
-algoCombobox = Combobox(frame1, state='readonly')
-algoCombobox.grid(column=3, row=9, columnspan=5)
+        scell[i].append(tk.Entry(group[int(i/3)][int(j/3)], width='4'))
+        scell[i][j].grid(column=i%3, row=j%3)
+
+statusLabelVar = tk.StringVar()
+statusLabelVar.set("Idle")
+statusLabel = tk.Label(frame2, textvariable=statusLabelVar)
+statusLabel.grid(column=0, row=0, sticky='w')
+
+solveBtn = tk.Button(frame2, text="Solve", command=solveBtnAction)
+solveBtn.grid(column=0, row=1, sticky='w')
+
+algoCombobox = ttk.Combobox(frame2, state='readonly')
+algoCombobox.grid(column=0, row=2, sticky='w')
 algoCombobox['values'] = ('backtracking', 'estochastic')
 algoCombobox.current(0)
+
+inputFileBtn = tk.Button(frame2, text="Open file", command=inputFileBtnAction)
+inputFileBtn.grid(column=0, row=3, sticky='w')
+
+outputFileBtn = tk.Button(frame2, text="Save file", command=outputFileBtnAction)
+outputFileBtn.grid(column=1, row=3, sticky='w')
+
 root.mainloop()
